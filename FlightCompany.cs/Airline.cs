1c264877448da2778df6a6768712c5934a5c2598
joel matsu <matsu.joel@gmail.com>
2017-04-28 02:45:48 -0400

    Airline.cs

--- FlightCompany.cs/Airline.cs
+++ FlightCompany.cs/Airline.cs
@@ -7,18 +7,31 @@ using System.Threading.Tasks;
 
 namespace FlightCompany.cs
 {
+    /// <summary>
+    /// Airline represents the company that uses the system, and is unique in the whole system.
+    /// Therefore, its only instance is encapsulated inside of a Singleton.
+    /// </summary>
     class Airline
     {
         private static Airline _instance;
-
+        /// <summary>
+        /// Represents the major name of the company.
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// Represents the physical address of the company.
+        /// </summary>
         public string Address { get; set; }
-
+        /// <summary>
+        /// Constructor is protected, in order to force the Singleton Pattern.
+        /// </summary>
         protected Airline()
         {
 
         }
-
+        /// <summary>
+        /// Returns the only instance of Airline that exists in the whole system.
+        /// </summary>
         public static Airline Instance
         {
             get
